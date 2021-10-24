da9b111e05a6c3ee53034f76c3a024c9e2869751
GarlicB <rockplani@gmail.com>
2021-10-24 17:00:53 +0900

    풀스택(리액트/스프링부트) 환경세팅

---
+++ src/main/java/com/example/webmusicsheet/WebmusicsheetApplication.java
@@ -0,0 +1,13 @@
+package com.example.webmusicsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmusicsheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebmusicsheetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webmusicsheet/WebmusicsheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webmusicsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmusicsheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
